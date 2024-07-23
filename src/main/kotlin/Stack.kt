8f21df9889f96920994c0e9b4db6faac49c03f12
Beza Sirak <sirak.bezawit@gmail.com>
2024-07-23 11:37:55 -0400

    added src

---
+++ src/main/kotlin/Stack.kt
@@ -0,0 +1,24 @@
+class Stack {
+
+//  Stack:    Google --> Amazon --> ebay
+
+    fun main(args: Array<String>) {
+        println("Hello World!")
+
+        // Try adding program arguments via Run/Debug configuration.
+        // Learn more about running applications: https://www.jetbrains.com/help/idea/running-applications.html.
+        println("Program arguments: ${args.joinToString()}")
+
+    }
+
+//     Hi
+
+
+
+
+
+}
+class Node(
+    var node: String,
+    var next: Node
+)
